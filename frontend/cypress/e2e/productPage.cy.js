39c414cdb4735ea9735d92ad8e1033b3df4b858b
zepinheiro <josefpinheiro98@gmail.com>
2022-06-04 17:53:58 +0100

    experiment with cypress

---
+++ frontend/cypress/e2e/productPage.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+describe("Add Product Test", () => {
+  it("should add a new product", () => {
+    cy.intercept("GET", "/api/products", { fixture: "products/empty.json" }).as(
+      "getProductsEmpty"
+    );
+
+    cy.intercept("POST", "/api/products", {
+      fixture: "products/newProduct.json",
+    }).as("postNewProduct");
+
+    cy.visit("http://localhost/");
+
+    cy.wait("@getProductsEmpty");
+
+    cy.get('[data-testid="add-product-form-title"]').contains(
+      "Add a new product"
+    );
+
+    cy.get('[data-testid="product-list-not-found"]').contains(
+      "No products found ..."
+    );
+
+    cy.get('[data-testid="input-form"]')
+      .type("NEW PRODUCT")
+      .should("have.value", "NEW PRODUCT");
+
+    cy.intercept("GET", "/api/products", {
+      fixture: "products/singleProduct.json",
+    }).as("getNewProduct");
+
+    cy.get('[data-testid="button-element"]').click();
+
+    cy.wait(["@postNewProduct", "@getNewProduct"]);
+
+    cy.get('[data-testid="product-name"]').contains("A NEW PRODUCT");
+  });
+});
